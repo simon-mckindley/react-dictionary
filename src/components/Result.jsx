906a519e5286218c7e8573ff3173d6a5c9bf0a70
SimonMck789 <S9406133@student.rmit.edu.au>
2025-05-05 23:39:10 +1000

    added synonyms, mobile styling

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,11 @@
 
+const Definition = ({ def }) => {
+    return (
+        <li className="definitions-item">
+            {def.definition}
+        </li>
+    );
+}
 
 export default function Result({ word, phonetics, meanings }) {
     if (!word) {
@@ -12,7 +19,7 @@ export default function Result({ word, phonetics, meanings }) {
                 <p><span className="bold move-in">Phonetics:</span> {phonetics.map(phonetic => phonetic.text).join(' | ')}</p>
             )}
 
-            <span className="bold">Meanings:</span>
+            <span className="bold">Meanings</span>
 
             {meanings && meanings.length > 0 && (
                 <ul className="meanings">
@@ -20,11 +27,15 @@ export default function Result({ word, phonetics, meanings }) {
                         <li className="meanings-item move-up" key={index}>
                             <span className="bold capitalise">{meaning.partOfSpeech}</span>:
                             <ul className="definitions">
-                                {meaning.definitions.map((def, index) => (
-                                    <li className="definitions-item" key={index}>
-                                        {def.definition}
-                                    </li>
+                                {(meaning.definitions ?? []).map((def, i) => (
+                                    <Definition def={def} key={i} />
                                 ))}
+                                {(meaning.synonyms ?? []).length > 0 && (
+                                    <p className="synonyms"><span className="bold">Synonyms:</span> {meaning.synonyms.join(", ")}</p>
+                                )}
+                                {(meaning.antonyms ?? []).length > 0 && (
+                                    <p className="antonyms"><span className="bold">Antonyms:</span> {meaning.antonyms.join(", ")}</p>
+                                )}
                             </ul>
                         </li>
                     ))}
